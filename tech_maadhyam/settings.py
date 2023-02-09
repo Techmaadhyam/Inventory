f8ec6cbdf7940835ac5292d61b74d54b4ab17960
Techmaadhyam <123808278+Techmaadhyam@users.noreply.github.com>
2023-02-10 01:15:13 +0530

    fixing issue

--- tech_maadhyam/settings.py
+++ tech_maadhyam/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'tech_maadhyam.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-     default': {
+     default: {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'inventory',
         'USER':'admin',
