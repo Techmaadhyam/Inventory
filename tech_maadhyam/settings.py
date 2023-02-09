69ee6b5faf2bb13868e553ff439db68fe50dda74
Techmaadhyam <123808278+Techmaadhyam@users.noreply.github.com>
2023-02-10 01:53:04 +0530

    fixing bug

--- tech_maadhyam/settings.py
+++ tech_maadhyam/settings.py
@@ -84,13 +84,20 @@ WSGI_APPLICATION = 'tech_maadhyam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    default : {
+#        'ENGINE': 'django.db.backends.postgresql',
+ #       'NAME': 'inventory',
+#        'USER':'admin',
+#        'PASSWORD':'admin',
+#        'PORT':'5432'
+#    }
+#}
+
 DATABASES = {
-    default : {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'inventory',
-        'USER':'admin',
-        'PASSWORD':'admin',
-        'PORT':'5432'
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
